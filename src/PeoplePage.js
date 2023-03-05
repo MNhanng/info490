640ffc964850a3c44aa6ba9c90e685f29c083f82
Nhan Nguyen <nhannguyen@Nhans-MacBook-Air-2.local>
2023-03-05 11:34:29 -0800

    filtering on people page

--- src/PeoplePage.js
+++ src/PeoplePage.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
 export function PeoplePage (props) {
-    // get users data in array form
-    const allProfiles = Object.values(props.usersData)
+    const [searchString, setSearchString] = useState("")
+    const [selectedRole, setSelectedRole] = useState("all")
+    const [selectedMajor, setSelectedMajor] = useState("all")
+    const [selectedIndustry, setSelectedIndustry] = useState("all")
+    const [selectedTag, setSelectedTag] = useState(false);
+    const allProfiles = props.usersData
 
     // filter for people based on search string
-    const [searchString, setSearchString] = useState("")
+
     const onChange = (event) => {
         setSearchString(event.target.value);
     }
@@ -14,11 +18,52 @@ export function PeoplePage (props) {
     };
     const filteredPeople = (allProfiles).filter(searchPeople);
 
+    // filter for people based selected filters and tags
+    const handleRoleClick = (event) => {
+        setSelectedRole(event.target.value);
+    }
+    const handleIndustryClick = (event) => {
+        setSelectedIndustry(event.target.value);
+    }
+    const handleMajorClick = (event) => {
+        setSelectedMajor(event.target.value);
+    }
+    const handleTagClick = () => {
+        setSelectedTag(!selectedTag);
+    }
+
+    const filterByRole = (people) => {
+        return people.role.includes(selectedRole);
+    }
+    const filterByIndustry = (people) => {
+        return people.industry.includes(selectedIndustry);
+    }
+    const filterByMajor = (people) => {
+        return people.major.includes(selectedMajor);
+    }
+    const filterByTag = (people) => {
+        return people.contact.open_contact === "Yes";
+    }
+
+    let filteredByFiltersPeople = filteredPeople;
+    if (selectedRole !== "all") {
+        filteredByFiltersPeople = filteredPeople.filter(filterByRole)
+    } 
+    if (selectedIndustry !== "all") {
+       filteredByFiltersPeople = filteredPeople.filter(filterByIndustry)
+    }
+    if (selectedMajor !== "all") {
+        filteredByFiltersPeople = filteredPeople.filter(filterByMajor)
+    }
+    if (selectedTag === true) {
+        filteredByFiltersPeople = filteredPeople.filter(filterByTag)
+    }
+
     return (
         <main>
             <h1>Profiles</h1>
-            <PeopleSearchFilter onChange={onChange}/>
-            <Profiles usersData={filteredPeople}/>
+            <PeopleSearchFilter onChange={onChange} handleRoleClick={handleRoleClick} handleIndustryClick={handleIndustryClick} handleMajorClick={handleMajorClick} handleTagClick={handleTagClick} />
+            <Profiles usersData={filteredByFiltersPeople}/>
         </main>
     )
 }
@@ -73,27 +118,40 @@ function PeopleSearchFilter (props) {
                 <h2>Filter</h2>
                 <div className="people-filters">
                     <div>
-                        <select name="user_type" id="user_type">
+                        <select name="user_type" id="user_type" onChange={props.handleRoleClick} >
+                            <option value="all" selected>Role</option>
                             <option value="Student">Student</option>
                             <option value="Alumni">Alumni</option>
                         </select>
                     </div>
                     <div>
-                        <select name="major" id="major">
-                            <option value="Major">Major</option>
+                        <select name="major" id="major" onChange={props.handleMajorClick}>
+                            <option value="all" selected>Major</option>
+                            <option value="Art">Art</option>
+                            <option value="Business">Business</option>
+                            <option value="Biology">Biology</option>
+                            <option value="Chemisty">Chemistry</option>
+                            <option value="Economics">Economics</option>
+                            <option value="Computer Science">Computer Science</option>
+                            <option value="Public Health">Public Health</option>
+                            <option value="Nursing">Nursing</option>
                             <option value="Informatics">Informatics</option>
                         </select>
                     </div>
                     <div>
-                        <select name="industry" id="industry">
-                            <option value="Industry">Industry</option>
+                        <select name="industry" id="industry" onChange={props.handleIndustryClick}>
+                            <option value="all" selected>Industry</option>
                             <option value="Technology">Technology</option>
                             <option value="Healthcare">Healthcare</option>
                             <option value="Business">Business</option>
+                            <option value="Engineering">Engineering</option>
+                            <option value="Entertainment">Entertainment</option>
+                            <option value="Education">Education</option>
+                            <option value="Real Estate">Real Estate</option>
                         </select>
                     </div>
                     <div>
-                        <input className="open_contact" type="button" value="Open To Contact" />
+                        <input className="open_contact" type="button" value="Open To Contact" onClick={props.handleTagClick}/>
                     </div>
                 </div>
             </div>
