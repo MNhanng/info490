65ddf8dcae0d5a414d8513db9a605a2bc2a96a56
nhanng <nhanng@uw.edu>
2023-05-01 17:46:18 -0700

    more styling

--- src/MessagesPage.js
+++ src/MessagesPage.js
@@ -9,11 +9,11 @@ export function MessagesPage(props) {
                     </form>
 
                     <div class="message-preview">
-                        <div class="message-img"><img src={require("./img/user-img.jpg")} alt="user profile" /></div>
+                        <div class="message-img"><img src={require("./img/ava.jpg")} alt="user profile" /></div>
                         <div>
-                            <div class="message-name">Username</div>
-                            <div class="message-time">Message date | Message time</div>
-                            <div class="message-desc">Some message preview kfishfohsrhgosrhr lsfhrk lscirsjoihfoso rsurhfou hseslnkl sndjlnvl shvlhzrl hflshohfosheor</div>
+                            <div class="message-name">Ava Brown</div>
+                            <div class="message-time">May 1, 2023 | 2:00pm</div>
+                            <div class="message-desc">Hi there, I will send you additional details about the internship requirements</div>
                         </div>
                     </div>
 
@@ -22,9 +22,9 @@ export function MessagesPage(props) {
                     <div class="message-preview">
                         <div class="message-img"><img src={require("./img/user-img.jpg")} alt="user profile" /></div>
                         <div>
-                            <div class="message-name">Username</div>
-                            <div class="message-time">Message date | Message time</div>
-                            <div class="message-desc">Some message preview kfishfohsrhgosrhr lsfhrk lscirsjoihfoso rsurhfou hseslnkl sndjlnvl shvlhzrl hflshohfosheor</div>
+                            <div class="message-name">David Smith</div>
+                            <div class="message-time">May 1, 2023 | 2:00pm</div>
+                            <div class="message-desc">Do you have any tips for my upcoming interview?</div>
                         </div>
                     </div>
 
@@ -33,9 +33,9 @@ export function MessagesPage(props) {
                     <div class="message-preview">
                         <div class="message-img"><img src={require("./img/user-img.jpg")} alt="user profile" /></div>
                         <div>
-                            <div class="message-name">Username</div>
-                            <div class="message-time">Message date | Message time</div>
-                            <div class="message-desc">Some message preview kfishfohsrhgosrhr lsfhrk lscirsjoihfoso rsurhfou hseslnkl sndjlnvl shvlhzrl hflshohfosheor</div>
+                            <div class="message-name">Linda Johnson</div>
+                            <div class="message-time">May 1, 2023 | 2:00pm</div>
+                            <div class="message-desc">I would love to learn more about it. Talk to you later.</div>
                         </div>
                     </div>
 
@@ -45,17 +45,17 @@ export function MessagesPage(props) {
         
                 
                 <div class="message-content">
-                    <div class="message-content-name">Name</div>
+                    <div class="message-content-name">Ava Brown</div>
                     <hr />
 
                     <div class="received-message">
                         <img src={require("./img/user-img.jpg")} alt="user profile" />
-                        <div class="message">Some message lshcosrufy kschrkhkf</div>
+                        <div class="message">Hello, I would like to learn more about the internship that you posted about!</div>
                     </div>
 
                     <div class="sent-message">
                         <img src={require("./img/user-img.jpg")} alt="user profile" />
-                        <div class="message">Some message lshcosrufy kschrkhkf</div>
+                        <div class="message">Hi there, I will send you additional details about the internship requirements</div>
                     </div>
 
                     <div class="add-message-container">
